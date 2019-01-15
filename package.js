c3c767f8188471875e9d49505e67d1fa32e0b9f2
Axel Vaindal <pro.axelvaindal@gmail.com>
2019-01-15 21:33:45 +0100

    dev: Add xo linter dependency
    xo is now configured to automaticaly be applied on changes made to source code in this repository.

--- package.js
+++ package.js
@@ -1,34 +1,31 @@
-Package.describe(
-{
-    name: "m4dnation:accounts-twitch",
-        
-    version: "0.0.1",
-        
-    summary: "A login service for Twitch.",
-        
-    git: "https://github.com/M4dNation/accounts-twitch",
+Package.describe({
+  name: 'm4dnation:accounts-twitch',
+
+  version: '0.0.1',
+
+  summary: 'A login service for Twitch.',
+
+  git: 'https://github.com/M4dNation/accounts-twitch',
 });
-        
-Package.onUse(function(api)
-{
-    api.versionsFrom("1.7.0.3");
-        
-    api.use("ecmascript");
-    api.use("accounts-base", ["client", "server"]);
-    api.imply("accounts-base");
-    api.use("accounts-oauth", ["client", "server"]);
 
-    api.use("oauth", ["client", "server"]);
-    api.use("oauth2", ["client", "server"]);
-    api.use("http", ["server"]);
-    api.use("underscore", "server");
-    api.use("random", "client");
-    api.use("service-configuration", ["client", "server"]);
+Package.onUse(function(api) {
+  api.versionsFrom('1.7.0.3');
 
-    api.addFiles("src/accounts-twitch-client.js", "client");
-    api.addFiles("src/accounts-twitch-server.js", "server");
-    api.addFiles("src/accounts-twitch.js");
+  api.use('ecmascript');
+  api.use('accounts-base', ['client', 'server']);
+  api.imply('accounts-base');
+  api.use('accounts-oauth', ['client', 'server']);
 
-    api.export("Twitch");
+  api.use('oauth', ['client', 'server']);
+  api.use('oauth2', ['client', 'server']);
+  api.use('http', ['server']);
+  api.use('underscore', 'server');
+  api.use('random', 'client');
+  api.use('service-configuration', ['client', 'server']);
+
+  api.addFiles('src/accounts-twitch-client.js', 'client');
+  api.addFiles('src/accounts-twitch-server.js', 'server');
+  api.addFiles('src/accounts-twitch.js');
+
+  api.export('Twitch');
 });
-        
--- src/accounts-twitch.js
+++ src/accounts-twitch.js
@@ -1,29 +1,24 @@
-Accounts.oauth.registerService("twitch");
+Accounts.oauth.registerService('twitch');
 
-if (Meteor.isClient)
-{
-    Meteor.loginWithTwitch = function(options, callback) 
-    {
-        if (! callback && typeof options === "function") 
-        {
-            callback = options;
-            options = null;
-        }
+if (Meteor.isClient) {
+  Meteor.loginWithTwitch = function(options, callback) {
+    if (!callback && typeof options === 'function') {
+      callback = options;
+      options = null;
+    }
 
-        var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
-        Twitch.requestCredential(options, credentialRequestCompleteCallback);
-    };
-} 
-else 
-{
-    Accounts.addAutopublishFields(
-    {
-        forLoggedInUser: ["services.twitch"],
-        forOtherUsers: 
-        [
-            "services.twitch.display_name",
-            "services.twitch.name",
-            "services.twitch.logo"
-        ]
-    });
+    var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(
+      callback
+    );
+    Twitch.requestCredential(options, credentialRequestCompleteCallback);
+  };
+} else {
+  Accounts.addAutopublishFields({
+    forLoggedInUser: ['services.twitch'],
+    forOtherUsers: [
+      'services.twitch.display_name',
+      'services.twitch.name',
+      'services.twitch.logo',
+    ],
+  });
 }
